e780b2a2b9c0b01d5bd63c1b594443ce0678c46e
Ryan Parker <rparker52003@gmail.com>
2024-02-04 02:51:59 -0800

    Completed Python Testing Lab from Prof. John Businge in CS472-1001 Spring 2024

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,69 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+from src.counter import COUNTERS
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should update a counter"""
+        # 1. Make a call to Create a counter.
+        result = self.client.post('/counters/update')
+
+        # 2. Ensure that it returned a successful return code.
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+        # 3. Check the counter value as a baseline.
+        baseline_value = result.get_json()['update']
+        self.assertEqual(baseline_value, 0)
+
+        # 4. Make a call to Update the counter that you just created.
+        updated_result = self.client.put('/counters/update')
+        
+        # 5. Ensure that it returned a successful return code.
+        self.assertEqual(updated_result.status_code, status.HTTP_200_OK)
+
+        # 6. Check that the counter value is one more than the baseline you measured in step 3.
+        updated_value = updated_result.get_json()['update']
+        self.assertEqual(updated_value, baseline_value+1)
+
+        # Check that updating a new client returns a proper return code.
+        result2 = self.client.put('/counters/update_fail')
+        self.assertEqual(result2.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_read_a_counter(self):
+        """It should read a counter"""
+        # Make a call to create a counter, and put the counter
+        self.client.post('/counters/read')
+        self.client.put('/counters/read')
+
+        # Make a call to get a counter, and ensure it returned a good code
+        result = self.client.get('/counters/read')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+
+        # Check that reading a fake client returns a proper return code.
+        result2 = self.client.get('/counters/read_fail')
+        self.assertEqual(result2.status_code, status.HTTP_404_NOT_FOUND)
